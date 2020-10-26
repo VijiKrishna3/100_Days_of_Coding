8dd35ddaff520b65032dbef6b5eefa00aef50ef8
dimitri-dev <58113389+dimitri-dev@users.noreply.github.com>
2020-10-27 00:22:24 +0100

    Day 11 - solved
    Fully implemented solution of the day 11 task

---
+++ Day 11 - Easy/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _100daysCoding
+{
+    class Program_Day11
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input a number of elements in the list: ");
+            int nElements = Convert.ToInt32(Console.ReadLine());
+
+            int[] array = new int[nElements];
+            string output = "";
+
+            for (int i = 0; i < nElements; ++i)
+            {
+                Console.Write($"Input the {i + 1}. element: ");
+                array[i] = Convert.ToInt32(Console.ReadLine());
+
+                // Auto swapping every second element
+                if (i % 2 == 1)
+                {
+                    (array[i - 1], array[i]) = (array[i], array[i - 1]);
+                    output += array[i - 1] + " -> " + array[i];
+
+                    if (i != nElements - 1)
+                    {
+                        output += " -> ";
+                    }
+                }
+            }
+
+            Console.WriteLine("\n" + output);
+            Console.ReadKey();
+            
+        }
+    }
+}
