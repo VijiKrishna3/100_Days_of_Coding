14b14fa40725c7db950a2005cf1afb7bd441d0f7
dimitri-dev <58113389+dimitri-dev@users.noreply.github.com>
2020-10-26 23:51:42 +0100

    Day 9 - solved
    Fully implemented solution of the day 9 task

---
+++ Day 9 - Easy/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _100daysCoding
+{
+    class Program_Day8
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input a number of elements in an array: ");
+            int nElements = Convert.ToInt32(Console.ReadLine());
+
+            int[] array = new int[nElements];
+
+            for (int i = 0; i < nElements; ++i)
+            {
+                Console.Write($"Input the {i + 1}. element: ");
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            // Properly initializing Dictionary
+            SortedDictionary<int, int> positions = new SortedDictionary<int, int>();
+            for (int i = 0; i < nElements; ++i)
+            {
+                positions[array[i]] = 0;
+            }
+
+            // EndValue data and a temporary variable placeholder initialized
+            int endVal = 0, temp = 0;
+
+            for (int i = 0; i < nElements; ++i)
+            {
+                temp = Math.Max(temp, positions[array[i]]);
+                positions[array[i]] = i + 1;
+                endVal = Math.Max(endVal, i - temp + 1);
+            }
+
+            Console.WriteLine($"The length of the largest subarray of distinct elements is {endVal}.");
+
+            Console.ReadLine();
+        }
+    }
+}
