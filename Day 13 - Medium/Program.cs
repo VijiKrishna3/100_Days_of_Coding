de7197930c4beb6a9f877b537c917d9c870b1036
dimitri-dev <58113389+dimitri-dev@users.noreply.github.com>
2020-10-28 23:16:31 +0100

    Day 13 - solved
    Solved maximum subarray task for day 13.

---
+++ Day 13 - Medium/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _100daysCoding
+{
+    class Program_Day13
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input a number of elements in the array: ");
+            int nElements = Convert.ToInt32(Console.ReadLine());
+
+            int[] array = new int[nElements];
+            int minimum = 0, sum_arr = 0, num_of_negs = 0;
+
+            for (int i = 0; i < nElements; ++i)
+            {
+                Console.Write($"Input the {i + 1}. element: ");
+                array[i] = Convert.ToInt32(Console.ReadLine());
+
+                if (array[i] < 0)
+                {
+                    ++num_of_negs;
+                    if (array[i] < minimum)
+                    {
+                        minimum = array[i];
+                    }
+                }
+
+                sum_arr += array[i];
+            }
+
+            Console.WriteLine($"The maximum subarray sum equals {sum_arr - minimum}.");
+            Console.ReadLine();
+        }
+    }
+}
