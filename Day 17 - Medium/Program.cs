3f3a8a3ab33354fe4e2fcde832e08e27d3dabd89
dimitri-dev <58113389+dimitri-dev@users.noreply.github.com>
2020-10-30 18:10:56 +0100

    Day 17 - solved
    Solved GCD task with 2 methods (overload used).

---
+++ Day 17 - Medium/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_17___Medium
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter all the integers that you want to know the GCD of in a line: ");
+            string _s = Console.ReadLine();
+            int[] arr = _s.Split(' ').Select(o => Convert.ToInt32(o)).ToArray();
+
+            Console.WriteLine($"{GCD(arr)}");
+            Console.ReadLine();
+        }
+
+        static int GCD(int[] numbers)
+        {
+            return numbers.Aggregate(GCD);
+        }
+
+        static int GCD(int a, int b)
+        {
+            return b == 0 ? a : GCD(b, a % b);
+        }
+    }
+}
