b8483f547518bad1fcf618545fad9d5a57d3d16e
dimitri-dev <58113389+dimitri-dev@users.noreply.github.com>
2020-11-02 16:20:29 +0100

    Day 20 - solved
    Solved the task using a different approach that will print out every possible subset if there are multiple biggest subsets that have the same lenght.

--- Day 020 - Medium/Program.cs
+++ Day 020 - Medium/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,12 +15,19 @@ namespace _100daysCoding
             string _s = Console.ReadLine();
             int[] arr = _s.Split(' ').Select(o => Convert.ToInt32(o)).ToArray();
 
+            Solution2(arr);
+            Console.ReadLine();
+        }
+
+        // This solution only works if the given array has only 1 unique-sized big subset
+        public static void OriginalSolution(int[] arr)
+        {
             var subsets = arr.ToDictionary(key => key, value => 0);
 
             // So if a number can be devided by a subset, we increase the number's value
             // Whatever value has the most elements in the end is the biggest subset
 
-            for(int i = 0; i < arr.Length; ++i)
+            for (int i = 0; i < arr.Length; ++i)
             {
                 for (int j = 0; j < arr.Length; ++j)
                 {
@@ -60,6 +68,56 @@ namespace _100daysCoding
                 Console.Write($"{ val.Key} ");
 
             Console.ReadLine();
+        }
+
+        // This solution will calculate every subset into a specific array using a sieve
+        public static void Solution2(int[] arr)
+        {
+            Array.Sort(arr);
+            var subsets = arr.ToDictionary(Key => Key, Value => 0);
+
+            // This is basically like a sieve -> say we have 3 6 9 5 10 20,
+            // it's going to take 3 and mark 6 9 to be a subset of 3, then take 5 and mark 10 20 to be a subset of 5
+            for (int i = 0; i < arr.Length; ++i)
+            {
+                for (int j = arr[i]; j <= arr.Last(); j+=arr[i])
+                {
+                    if (subsets.ContainsKey(j) == true && subsets[j] == 0)
+                        subsets[j] = arr[i];
+                }
+            }
+
+            List<List<int>> allSubsets = new List<List<int>>();
+
+            var _nFreq = (from entry in subsets where entry.Value == entry.Value orderby entry.Value ascending select entry).ToList();
+
+            while (_nFreq.Count() != 0)
+            {
+                allSubsets.Add((from entry in _nFreq where entry.Value == _nFreq.First().Value select entry.Key).ToList());
+                _nFreq = _nFreq.Where(c => c.Value != _nFreq.First().Value).ToList();
+
+            }
+
+            // Assume the first array is the biggest because of the descending sort
+            int maxSubsetVal = allSubsets[0].Count();
+            int x = 0;
+
+            for (int i = 0; i < allSubsets.Count(); ++i)
+            {
+                if (allSubsets[i].Count() == maxSubsetVal)
+                {
+                    Console.WriteLine($"\nSubset n{x + 1}: ");
+                    for (int j = 0; j < allSubsets[i].Count(); ++j)
+                    {
+                        Console.Write($"{allSubsets[i][j]} ");
+                    }
+                    Console.WriteLine();
+                    ++x;
+                }
+            }
+
+            var _nFinal = from entry in _nFreq where entry.Value == _nFreq.First().Value select entry;
+
         }
     }
 }
