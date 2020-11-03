51f891483b7fd2cd407519dd3897bb02be954003
dimitri-dev <58113389+dimitri-dev@users.noreply.github.com>
2020-11-03 19:40:29 +0100

    Day 21 - solved
    Solved day 21 task using String.Replace funcitons to swap 0 and 1 bits.

---
+++ Day 021 - Medium/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _100daysCoding
+{
+    class Program_Day21
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input the 8-bit number (i.e. 01010101): ");
+            string s = Console.ReadLine();
+            s = s.Replace('0', '"').Replace('1', '0').Replace('"', '1');
+            Console.WriteLine($"Output: {s}");
+            Console.ReadLine();
+        }
+    }
+}
