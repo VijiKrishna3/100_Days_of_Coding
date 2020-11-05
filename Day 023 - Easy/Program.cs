ea238a64278432468678bbdd59e2e3cd8e806bb3
dimitri-dev <58113389+dimitri-dev@users.noreply.github.com>
2020-11-05 15:50:58 +0100

    Day 23 - solution
    Solution implemented with Combinatrics package ( https://github.com/eoincampbell/combinatorics )

---
+++ Day 023 - Easy/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Combinatorics.Collections;
+
+namespace _100daysCoding
+{
+    class Program_Day23
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input a string: ");
+            string s = Console.ReadLine();
+            var sC = (from entry in s select entry).ToList();
+
+            bool boolSol = false;
+            var p = new Permutations<char>(sC, GenerateOption.WithoutRepetition);
+            string temp = "", ts = "";
+
+            foreach (var v in p)
+            {
+                foreach (var k in v)
+                {
+                    temp += k;
+                }
+
+                ts = new string(temp.ToCharArray().Reverse().ToArray());
+
+                if (temp == ts)
+                {
+                    boolSol = true;
+                    break;
+                }
+
+                temp = "";
+            }
+                
+            Console.WriteLine($">< {boolSol} ><");
+            Console.ReadLine();
+        }
+    }
+}
