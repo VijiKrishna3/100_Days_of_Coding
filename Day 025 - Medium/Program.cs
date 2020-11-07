2890a0ff4b3f533ac611780350e4e0559cce8827
dimitri-dev <58113389+dimitri-dev@users.noreply.github.com>
2020-11-07 13:10:13 +0100

    Day 25 - solved
    Solved using simple linq queries (3 in total :+1: )

---
+++ Day 025 - Medium/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _100daysCoding
+{
+    class Program_Day25
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input all elements in a sequence: ");
+            var split = Console.ReadLine().Split(' ');
+            var intArr = (from entry in split select Convert.ToInt32(entry)).ToArray(); 
+
+            var dictInt = intArr.GroupBy(x => x).ToDictionary(x => x.Key, o => o.Count());
+
+            var duplicateResult = (from entry in dictInt where entry.Value == 2 select entry).ToList();
+            try
+            {
+                Console.WriteLine($"Duplicate: {duplicateResult[0].Key}");
+            }
+            catch { }
+            Console.ReadLine();
+        }
+    }
+}
