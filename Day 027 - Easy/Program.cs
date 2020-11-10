eee748145bbef06d776aed23ba8a7f4d2d6b4870
dimitri-dev <58113389+dimitri-dev@users.noreply.github.com>
2020-11-10 10:46:35 +0100

    Day 27 - solved
    Solved using old Day 21 solution that I mistakenly did

---
+++ Day 027 - Easy/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _100daysCoding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input a 32-bit number (i.e. 1111 0000 1111 0000 1111 0000 1111 0000): ");
+            string s = Console.ReadLine();
+            s = s.Replace('0', '"').Replace('1', '0').Replace('"', '1');
+            Console.WriteLine($"Output: {s}");
+            Console.ReadLine();
+        }
+    }
+}
