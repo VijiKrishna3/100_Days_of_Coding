e2afce2a41618162805aa14aa672c7ff6d3462a7
dimitri-dev <58113389+dimitri-dev@users.noreply.github.com>
2020-11-11 18:59:30 +0100

    Day 28 - solved
    Day 28 solved using .NET "Core" (.NET 5)

---
+++ Day 028 - Medium/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _100daysCoding
+{
+    class Program_Day28
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter n: ");
+            Int64 n = Convert.ToInt64(Console.ReadLine());
+
+            Console.WriteLine($"The square root of {n} is {Math.Sqrt(n)}.");
+        }
+    }
+}
