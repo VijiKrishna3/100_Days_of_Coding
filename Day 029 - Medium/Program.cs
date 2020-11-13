41990dba3c5d641dbe3ef3797551b7001bb375d7
dimitri-dev <58113389+dimitri-dev@users.noreply.github.com>
2020-11-13 12:32:56 +0100

    Day 29 - solution in progress of making
    Day 29 - not solved yet - but very close

---
+++ Day 029 - Medium/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _100daysCoding
+{
+    class Program_Day29
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of bits: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            var res = gray_code(n);
+            foreach (var x in res)
+                Console.WriteLine(x);
+        }
+
+        static List<string> gray_code(int n)
+        {
+            List<string> result = new List<string>();
+
+            if (n == 0)
+            {
+                return result;
+            }
+            else if (n == 1)
+            {
+                result.Add("0");
+                result.Add("1");
+                return result;
+            }
+
+            int cntr = 0;
+            while (cntr != Math.Pow(2, n))
+            {
+                ++cntr;
+            }
+
+            /* Permutations for 2bit with 2 for loops
+             * 
+             * var str = "";
+                            str += x;
+                            str += y;
+                            result.Add(str);
+                            ++cntr; */
+
+
+            return result;
+        }
+    }
+}
