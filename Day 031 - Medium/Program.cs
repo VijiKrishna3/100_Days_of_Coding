5f2c2e7da619ad9ee76a489294e27832133100cb
dimitri-dev <58113389+dimitri-dev@users.noreply.github.com>
2020-11-17 15:39:22 +0100

    Day 31 - solution in progress
    Made it so the function prints links for the nodes which are generated with random values and random pointers each time in app.

---
+++ Day 031 - Medium/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace _100daysCoding
+{
+    class Program_Day31
+    {
+        public class Node
+        {
+            public int data;
+            public Node sequence_next;
+            public Node sequence_random;
+
+            public Node(int data)
+            {
+                this.data = data;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var nodes = new Node[5];
+            Random rnd = new Random();
+            
+            for (int i = 1; i <= 5; ++i)
+                nodes[i - 1] = new Node(i);
+
+            for (int i = 0; i < 4; ++i)
+                nodes[i].sequence_next = nodes[i + 1];
+                
+            for (int i = 0; i < 5; ++i)
+                nodes[i].sequence_random = nodes[rnd.Next(0, 4)];
+
+
+            nodePrintLinks(nodes[0]);
+        }
+
+        static void nodePrintLinks(Node main)
+        {
+            Console.WriteLine("Node value : Random pointer node value");
+
+            var temporaryNode = main;
+            while (temporaryNode != null)
+            {
+                Console.WriteLine($"{temporaryNode.data} ({temporaryNode.sequence_random.data})");
+                temporaryNode = temporaryNode.sequence_next;
+            }
+        }
+    }
+}
