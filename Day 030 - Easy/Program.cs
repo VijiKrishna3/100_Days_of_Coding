74063986403e7abd4ae86c617715686311a1088d
dimitri-dev <58113389+dimitri-dev@users.noreply.github.com>
2020-11-17 14:57:19 +0100

    Day 30 - solution
    Day 30 solved using simple string logic.

---
+++ Day 030 - Easy/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _100daysCoding
+{
+    class Program_Day30
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter string one: ");
+            string s1 = Console.ReadLine();
+
+            Console.WriteLine("\nEnter string two: ");
+            string s2 = Console.ReadLine();
+
+            Console.WriteLine($"\nNumber of needed changes is {computeChanges(s1, s2)}.");
+        }
+
+        static int computeChanges(string s1, string s2)
+        {
+            int changes_min = (Math.Max(s1.Length, s2.Length));
+
+            if (s1.Length > s2.Length)
+                (s1, s2) = (s2, s1);
+
+
+            foreach (var c in s1)
+            {
+                if (s2.Contains(c))
+                    --changes_min;
+            }
+
+            return changes_min;
+        }
+    }
+}
