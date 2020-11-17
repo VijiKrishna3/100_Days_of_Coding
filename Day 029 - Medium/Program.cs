45baa70b730aa0244c59c5b0a6817e3598d44249
dimitri-dev <58113389+dimitri-dev@users.noreply.github.com>
2020-11-17 15:18:31 +0100

    Day 29 - fully solved
    Solved problem from day 29 using recursion in graycode generation.

--- Day 029 - Medium/Program.cs
+++ Day 029 - Medium/Program.cs
@@ -10,14 +10,14 @@ namespace _100daysCoding
             Console.Write("Enter the number of bits: ");
             int n = Convert.ToInt32(Console.ReadLine());
 
-            var res = gray_code(n);
+            var res = sortList(gray_code(n), n);
             foreach (var x in res)
                 Console.WriteLine(x);
         }
 
-        static List<string> gray_code(int n)
+        static List<int> gray_code(int n)
         {
-            List<string> result = new List<string>();
+            List<int> result = new List<int>();
 
             if (n == 0)
             {
@@ -25,27 +25,43 @@ namespace _100daysCoding
             }
             else if (n == 1)
             {
-                result.Add("0");
-                result.Add("1");
+                result.Add(0);
+                result.Add(1);
                 return result;
             }
 
-            int cntr = 0;
-            while (cntr != Math.Pow(2, n))
+            var previous = gray_code(n - 1);
+            result.AddRange(previous);
+
+            for (int i = previous.Count - 1; i > -1; --i)
             {
-                ++cntr;
+                result.Add((1 << (n - 1)) | previous[i]);
             }
 
-            /* Permutations for 2bit with 2 for loops
-             * 
-             * var str = "";
-                            str += x;
-                            str += y;
-                            result.Add(str);
-                            ++cntr; */
-
-
             return result;
+        }
+
+        static List<string> sortList(List<int> k, int n)
+        {
+            List<string> sorted = new List<string>();
+
+            foreach(var x in k)
+            {
+                if ((Convert.ToString(x, 2).Length == n))
+                    sorted.Add(Convert.ToString(x, 2));
+                else
+                {
+                    int kx = Convert.ToString(x, 2).Length;
+                    string s = "";
+                    for (int y = 0; y < (n - kx); ++y)
+                        s += "0";
+
+                    s += Convert.ToString(x, 2);
+                    sorted.Add(s);
+                }
+            }
+
+            return sorted;
         }
     }
 }
