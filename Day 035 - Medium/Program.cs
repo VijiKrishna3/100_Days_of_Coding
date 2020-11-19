444601e72a497ba38051b8936255b7d14eb32fe9
dimitri-dev <58113389+dimitri-dev@users.noreply.github.com>
2020-11-19 21:31:26 +0100

    Day 35 - solved
    Implemented the singleton pattern with a twist as requested.

---
+++ Day 035 - Medium/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _100daysCoding
+{
+    class Program_Day35
+    {
+        public sealed class Singleton
+        {
+            private static Singleton InstanceEven = new Singleton();
+            private static Singleton InstanceOdd = new Singleton();
+
+            private static int counter = 0;
+
+            private Singleton() { }
+
+            public static Singleton getInstance()
+            {
+                ++counter;
+                if (counter % 2 == 1)
+                    return InstanceOdd;
+                else
+                    return InstanceEven;
+            }
+        }
+    }
+}
