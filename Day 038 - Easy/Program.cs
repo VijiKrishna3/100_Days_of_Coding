b9529d76f6144cd383af930e9ee3e6b39a5d8bf6
dimitri-dev <58113389+dimitri-dev@users.noreply.github.com>
2020-11-23 10:19:10 +0100

    Day 38 - solved
    Solved using the Combinatorics NuGet package.
    Also included a nice little dandy Stopwatch

---
+++ Day 038 - Easy/Program.cs
@@ -0,0 +1,36 @@
+﻿using Combinatorics.Collections;
+using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace _100daysCoding
+{
+    class Program_Day38
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input all digits in a line i.e. 123: ");
+            var n = Console.ReadLine();
+            var sN = (from entry in n select entry).ToList();
+
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            var p = new Permutations<char>(sN, GenerateOption.WithoutRepetition);
+            stopwatch.Stop();
+
+            string temp = "";
+
+            Console.WriteLine(stopwatch.Elapsed);
+            Console.WriteLine("\nHere are all the permutations:");
+            
+            foreach (var v in p)
+            {
+                temp = string.Join("", v);
+
+                Console.WriteLine(temp);
+
+                temp = "";
+            }
+        }
+    }
+}
