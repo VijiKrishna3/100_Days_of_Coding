993b720b9074a0817cffc528bab5d93f2777c73d
dimitri-dev <58113389+dimitri-dev@users.noreply.github.com>
2020-11-28 10:15:26 +0100

    Day 43 - solved
    Solved simple 50/50 coin flip task using log2(n)

---
+++ Day 043 - Easy/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _100daysCoding
+{
+    class Program_Day43
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input n: ");
+            long n = Convert.ToInt64(Console.ReadLine());
+
+            Console.WriteLine($"The number of rounds equals {Math.Log2(n)}.");
+        }
+    }
+}
