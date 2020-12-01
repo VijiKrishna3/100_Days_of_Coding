999c56a23185dd8550113b9ec8f70d18def3c105
dimitri-dev <58113389+dimitri-dev@users.noreply.github.com>
2020-12-01 13:08:55 +0100

    Day 44 - solved
    Solved the "next permutation" problem of task 44.

---
+++ Day 044 - Hard/Program.cs
@@ -0,0 +1,35 @@
+﻿using Combinatorics.Collections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _100daysCoding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input all digits in a line i.e. 123: ");
+            var n = Console.ReadLine().ToList();
+
+            bool changes = false;
+            for (int i = n.Count - 1; i > 0; --i)
+            {
+                if (n[i - 1] < n[i])
+                {
+                    ++n[i - 1];
+                    --n[n.FindLastIndex(a => a == n[i - 1])];
+                    changes = true;
+                    n.Sort(i, n.Count - i, null);
+                    break;
+                }
+            }
+
+            if (!changes)
+                n.Reverse();
+
+            foreach (var x in n)
+                Console.Write($"{x}");
+        }
+    }
+}
