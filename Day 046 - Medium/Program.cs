10d10123c9a139738ae9497a7ad2537eb4854199
dimitri-dev <58113389+dimitri-dev@users.noreply.github.com>
2020-12-02 12:49:06 +0100

    Day 46 - solved (bugfixed)
    Missed a step in the task -.- dumb me

--- Day 046 - Medium/Program.cs
+++ Day 046 - Medium/Program.cs
@@ -9,13 +9,18 @@ namespace _100daysCoding
         {
             Console.WriteLine("Input the array in one line as in: 1 2 3 4");
             var ins = Console.ReadLine().Split(' ').ToList();
-            var integers = (from entry in ins orderby entry select Convert.ToInt32(entry)).ToList();
+            var integers = (from entry in ins orderby Convert.ToInt32(entry) select Convert.ToInt32(entry)).ToList();
 
             Console.Write("Input index of number: ");
             int idx = Convert.ToInt32(Console.ReadLine());
 
             int sortedIndexNext = integers.FindIndex(p => p == Convert.ToInt32(ins[idx])) + 1;
-            Console.WriteLine($"{ins.FindIndex(b => b == integers[sortedIndexNext].ToString())}");
+
+            // Means the element is the greatest
+            if (sortedIndexNext >= integers.Count)
+                Console.WriteLine("null");
+            else
+                Console.WriteLine($"{ins.FindIndex(b => b == integers[sortedIndexNext].ToString())}");
         }
     }
 }
