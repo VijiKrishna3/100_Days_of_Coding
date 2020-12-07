4081188b7ffc6c7d87879ab1c1d22350af053228
dimitri-dev <58113389+dimitri-dev@users.noreply.github.com>
2020-12-07 10:55:43 +0100

    Day 50 - solved :beers:
    Solved day 50 task! Half way there!!!

---
+++ Day 050 - Easy/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace _100daysCoding
+{
+    class Program_Day50
+    {
+        static void Main(string[] args)
+        {
+            char[,] matrix = {{ 'F', 'A', 'C', 'I' },
+                              { 'O', 'B', 'Q', 'P' },
+                              { 'A', 'N', 'O', 'B' },
+                              { 'M', 'A', 'S', 'S' }};
+
+            Console.WriteLine("Input a word: ");
+            string w = Console.ReadLine();
+            bool found = false;
+
+            // Square matrix
+            int sizeOfMatrix = (int)Math.Sqrt(matrix.Length);
+
+            // Check every row and column
+            for (int i = 0; i < sizeOfMatrix; ++i)
+            {
+                string wholeRow = new string(Enumerable.Range(0, sizeOfMatrix).Select(x => matrix[i, x]).ToArray());
+                string wholeCol = new string(Enumerable.Range(0, sizeOfMatrix).Select(x => matrix[x, i]).ToArray());
+
+                if (wholeRow.Contains(w) || wholeCol.Contains(w))
+                {
+                    found = true;
+                    break;
+                }
+            }
+
+            Console.WriteLine($"\nElement can be found: {found}");
+        }
+    }
+}
