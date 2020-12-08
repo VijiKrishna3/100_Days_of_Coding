af687be1da31f76edc8610f4f5c530c15569e44f
dimitri-dev <58113389+dimitri-dev@users.noreply.github.com>
2020-12-08 11:43:46 +0100

    Day 51 - solved

---
+++ Day 051 - Easy/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _100daysCoding
+{
+    class Program_Day51
+    {
+        static void Main(string[] args)
+        {
+            Debounce(someFunctionV, 420);
+        }
+
+        static void someFunctionV()
+        {
+            Console.WriteLine("Hello World.");
+        }
+
+        // Debounced returns a debounced function that will not call its
+        // provided function until after N miliseconds despite calling it.
+        static void Debounce(Action f, int N)
+        {
+            DateTime c = DateTime.UtcNow;
+            while ((DateTime.UtcNow - c).TotalMilliseconds < N) { }
+            f();
+        }
+    }
+}
