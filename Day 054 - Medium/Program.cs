56b7cd42eed382644b026e0baeb5a03ddac5c2c1
dimitri-dev <58113389+dimitri-dev@users.noreply.github.com>
2020-12-11 11:43:40 +0100

    Day 54 - solved
    Solved day 54 problem and since the problem states that the given solution could be one of them, so simple pivot-point sort is also a solution since this is a fully one-value LinkedList that we're manipulating (and can also be considered an array).

---
+++ Day 054 - Medium/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace _100daysCoding
+{
+    class Program_Day54
+    {
+        static void Main(string[] args)
+        {
+            // We will be creating the "linked list" of numbers as an iterating array
+            // i.e. 5 -> 1 -> 8 -> 0 -> 3
+            int[] arr = { 5, 1, 8, 0, 3 };
+
+            Console.Write("Input k: ");
+            int k = Convert.ToInt32(Console.ReadLine());
+
+            Array.Sort(arr, Comparer<int>.Create((x, y) => { return x < k ? -1 : 0; }));
+
+            foreach (var e in arr)
+                Console.Write($"{e} -> ");
+        }
+    }
+}
