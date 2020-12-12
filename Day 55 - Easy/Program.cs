97aacaf237b9e4486428634db54b0693e1c31de6
dimitri-dev <58113389+dimitri-dev@users.noreply.github.com>
2020-12-12 13:23:12 +0100

    Day 55 - solved
    Simple square the entire array and sort it problem

---
+++ Day 55 - Easy/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace _100daysCoding
+{
+    class Program_Day55
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input all the array numbers in one line");
+            var ins = Console.ReadLine().Split(' ').ToList();
+            var arr = (from entry in ins select (Convert.ToInt64(entry) * Convert.ToInt64(entry))).ToArray();
+            Array.Sort(arr);
+
+            foreach (var e in arr)
+                Console.Write($"{e} ");
+        }
+    }
+}
