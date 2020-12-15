f0d241e08bb8cdd5959ace4b4f10f6296eab745c
dimitri-dev <58113389+dimitri-dev@users.noreply.github.com>
2020-12-15 11:21:54 +0100

    Day 57 - solved
    Stock prices task

---
+++ Day 057 - Easy/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace _100daysCoding
+{
+    class ProgramDay57
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write the stock prices in chronological order: ");
+            var ins = Console.ReadLine().Split(' ').ToList();
+            var arr = (from entry in ins select Convert.ToInt64(entry)).ToList();
+
+            var idx = arr.FindIndex(f => f == arr.Min());
+            Console.WriteLine($"The most profitable option would be to buy them at {arr.Min()}, " +
+                              $"and sell them at {arr.GetRange(idx, arr.Count - idx).Max()}");
+        }
+    }
+}
