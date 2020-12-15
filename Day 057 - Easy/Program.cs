bdaaf626e4d235e6aeeda64e421edf86e0285865
dimitri-dev <58113389+dimitri-dev@users.noreply.github.com>
2020-12-15 12:53:52 +0100

    Day 57 - solved (fixed)
    Solved incorrectly / only for the specific case listed in the task. I'm a dum dum

--- Day 057 - Easy/Program.cs
+++ Day 057 - Easy/Program.cs
@@ -11,9 +11,16 @@ namespace _100daysCoding
             var ins = Console.ReadLine().Split(' ').ToList();
             var arr = (from entry in ins select Convert.ToInt64(entry)).ToList();
 
-            var idx = arr.FindIndex(f => f == arr.Min());
-            Console.WriteLine($"The most profitable option would be to buy them at {arr.Min()}, " +
-                              $"and sell them at {arr.GetRange(idx, arr.Count - idx).Max()}");
+            int bestProfitIndex = 0;
+            for (int idx = 1; idx < arr.Count; ++idx)
+            {
+                if (arr.GetRange(idx, arr.Count - idx).Max() - arr[idx] >
+                    arr.GetRange(bestProfitIndex, arr.Count - bestProfitIndex).Max() - arr[bestProfitIndex])
+                    bestProfitIndex = idx;
+            }
+
+            Console.WriteLine($"The most profitable option would be to buy them at {arr[bestProfitIndex]}, " +
+                              $"and sell them at {arr.GetRange(bestProfitIndex, arr.Count - bestProfitIndex).Max()}");
         }
     }
 }
