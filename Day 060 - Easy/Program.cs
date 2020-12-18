8ecef72c936258e54559fcde7f54d7ea2ba2c2bb
dimitri-dev <58113389+dimitri-dev@users.noreply.github.com>
2020-12-18 11:52:02 +0100

    Day 60 - solved
    Solved another bracket check task.

---
+++ Day 060 - Easy/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _100daysCoding
+{
+    class Program_Day60
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input a string to test: ");
+            string testString = Console.ReadLine();
+            Console.WriteLine($"String is balanced = {balanced(testString)}");
+        }
+
+        static bool balanced(string str)
+        {
+            Dictionary<char, char> brackets = new Dictionary<char, char>{ { ')', '(' }, { '}', '{' }, { ']', '[' } };
+            Stack<char> bracketStack = new Stack<char>();
+            foreach (char a in str)
+            {
+                if (a == '(' || a == '[' || a == '{')
+                    bracketStack.Push(a);
+                else if (a == ']' || a == ')' || a == '}')
+                {
+                    if (bracketStack.Count == 0) return false;
+                    if (bracketStack.Peek() == brackets[a]) bracketStack.Pop();
+                    else return false;
+                }
+            }
+            return bracketStack.Count == 0;
+        }
+    }
+}
