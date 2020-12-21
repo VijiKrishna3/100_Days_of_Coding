fdf98caba234e8cab43056fc02c1db2229f1c233
dimitri-dev <58113389+dimitri-dev@users.noreply.github.com>
2020-12-21 10:53:39 +0100

    Day 62 - solved
    Solved a simple count string iterations task.

---
+++ Day 062 - Easy/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _100daysCoding
+{
+    class Program_Day62
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input a string: ");
+            var ins = Console.ReadLine();
+
+            char currentChar = ins[0];
+            int currentCount = 0;
+            string reportAnalysis = "";
+
+            for (int i = 0; i < ins.Length; ++i)
+            {
+                if (ins[i] == currentChar) ++currentCount;
+                else
+                {
+                    reportAnalysis += Convert.ToString(currentCount) + currentChar;
+                    currentCount = 1;
+                    currentChar = ins[i];
+                }
+            }
+
+            reportAnalysis += Convert.ToString(currentCount) + currentChar;
+
+            Console.WriteLine($"{reportAnalysis}");
+        }
+    }
+}
