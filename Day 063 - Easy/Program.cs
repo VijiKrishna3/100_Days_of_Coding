de093f4a7b299271b940ba2102357364820c372e
dimitri-dev <58113389+dimitri-dev@users.noreply.github.com>
2020-12-22 18:35:58 +0100

    Day 63 - solved
    Simple bit/maths problem

---
+++ Day 063 - Easy/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _100daysCoding
+{
+    class Program_Day63
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter x: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter y: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter b: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Result: {returnFunct(x, y, b)}");
+        }
+
+        static int returnFunct(int a, int b, int x)
+        {
+            return (a * x) - b * (x - 1);
+        }
+    }
+}
