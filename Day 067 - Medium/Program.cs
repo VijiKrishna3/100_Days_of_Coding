2c27358f742e9c2ecdfb2fd4990e19ed98eb7d99
dimitri-dev <maric.neven03@gmail.com>
2020-12-26 11:27:08 +0100

    Day 67 - solved
    Solved an integer division without division/multiplication/modulus operators problem.

---
+++ Day 067 - Medium/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _100daysCoding
+{
+    class Program_Day67
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Integer a: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Integer b: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"{a}/{b} = {divide(a, b)}");
+        }
+
+        static int divide(int a, int b)
+        {
+            if (a < b || b == 0) return 0;
+
+            int div = 0;
+            while (a > b)
+            {
+                a -= b;
+                ++div;
+            }
+
+            return div;
+        }
+    }
+}
