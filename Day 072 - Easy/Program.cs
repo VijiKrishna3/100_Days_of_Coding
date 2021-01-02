80c9a2572a46c98a56f55e5e8b3cac86edc36714
dimitri-dev <maric.neven03@gmail.com>
2021-01-02 14:51:33 +0100

    Day 72 - solved
    Solved issue 72 - can the two strings be matched by shifting using StringBuilder

---
+++ Day 072 - Easy/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+
+namespace _100daysCoding
+{
+    class Program_Day72
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input string one: ");
+            string a = Console.ReadLine();
+
+            Console.Write("Input string two: ");
+            string b = Console.ReadLine();
+
+            Console.WriteLine($"Can the two strings be matched: {canBeMatched(a, b)}");
+        }
+
+        static bool canBeMatched(string a, string b)
+        {
+            if (a.Length != b.Length) return false;
+            if (a == b) return true;
+
+            StringBuilder sBuild = new StringBuilder(a); 
+            for (int i = a.Length - 1; i >= 0; --i)
+            {
+                sBuild.Insert(0, a[i]);
+                if (sBuild.ToString(0, a.Length) == b) return true;
+            }
+
+            return false;
+        }
+    }
+}
