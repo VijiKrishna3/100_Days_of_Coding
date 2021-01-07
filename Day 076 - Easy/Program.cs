b2f108b4841a3266c8b8ee98500c4d76a69b826d
dimitri-dev <maric.neven03@gmail.com>
2021-01-07 14:54:48 +0100

    Day 76 - solved
    Solved issue 75 - get nth fibonacci number

---
+++ Day 076 - Easy/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _100daysCoding
+{
+    class Program_Day76
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter which number of the Fibonnaci sequence do you want: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Your number is {Fib_N(n)}.");
+        }
+
+        static long Fib_N(int n)
+        {
+            if (n == 0) return 0;
+
+            long a = 0, b = 1;
+
+            for (int i = 2; i <= n; ++i)
+                (a, b) = (b, (a + b));
+
+            return b;
+        }
+    }
+}
