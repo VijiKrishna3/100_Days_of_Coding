d735b5b2b6a6d583dae1852cdeb6fcf9d86d94c8
dimitri-dev <maric.neven03@gmail.com>
2021-01-13 10:23:43 +0100

    Day 81 - solved
    Solved issue 80 - is it possible to hop through an array using element values

---
+++ Day 081 - Medium/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace _100daysCoding
+{
+    class Program_Day81
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insert all array elements in one line (i.e. 1 1 0 1)");
+            string[] ins = Console.ReadLine().Split(' ');
+            int[] arr = (from entry in ins select Convert.ToInt32(entry)).ToArray();
+
+            Console.WriteLine($"This array can be jumped through: {isJumpable(arr)}");
+        }
+
+        static bool isJumpable(int[] arr)
+        {
+            Array.Resize(ref arr, arr.Length - 1);
+
+            int steps = 0;
+            foreach (int x in arr)
+            {
+                steps = (steps > x) ? steps : x;
+                if (steps == 0) return false;
+                --steps;
+            }
+
+            return true;
+        }
+    }
+}
