249e6dba742bb5a9506c40837225b1d2ae40131c
dimitri-dev <maric.neven03@gmail.com>
2021-01-14 14:28:17 +0100

    Day 82 - solved
    Solved issue 82 - is number palindrome without strings

---
+++ Day 082 - Easy/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _100daysCoding
+{
+    class Program_Day82
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input n: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"{n} is a palindrome check says: {fancyPalindrome(n)}.");
+        }
+
+        static bool fancyPalindrome(int n)
+        {
+            List<int> num = new List<int>();
+            while (n != 0)
+            {
+                num.Add(n % 10);
+                n /= 10;
+            }
+
+            return Enumerable.SequenceEqual(num, num.ToArray().Reverse());
+        }
+    }
+}
