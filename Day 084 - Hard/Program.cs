c7dce7dba162ad6a7fbc8df2b284f3af444ad8ad
dimitri-dev <maric.neven03@gmail.com>
2021-01-16 12:50:35 +0100

    Day 84 - solved
    Solved issue 84 - get lowest number of squares summed to reach n

---
+++ Day 084 - Hard/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _100daysCoding
+{
+    class Program_Day84
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter n: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"You will need {muchoSquaro(n)} numbers squared to reach {n}.");
+        }
+
+        static int muchoSquaro(int n)
+        {
+            int[] dp = new int[n + 1];
+            for (int i = 1; i <= n; ++i)
+            {
+                dp[i] = int.MaxValue;
+                for (int j = 1; j * j <= i; ++j)
+                    dp[i] = (dp[i] < dp[i - j * j] + 1) ? dp[i] : dp[i - j * j] + 1;
+            }
+
+            return dp[n];
+        }
+    }
+}
