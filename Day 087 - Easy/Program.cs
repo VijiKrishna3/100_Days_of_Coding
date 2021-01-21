d26661e5cdd585c41dbdf77daeff4d4ca8d2f5f6
dimitri-dev <maric.neven03@gmail.com>
2021-01-21 09:20:20 +0100

    Day 87 - solved
    Solved issue 87 - rotate array around k being as lazy as possible

---
+++ Day 087 - Easy/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _100daysCoding
+{
+    class Program_Day87
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input the whole array in a single line (i.e. 5 4 7 2): ");
+            string[] ins = Console.ReadLine().Split(' ');
+            int[] arr = (from entry in ins select Convert.ToInt32(entry)).ToArray();
+
+            Console.Write("Enter k: ");
+            int k = Convert.ToInt32(Console.ReadLine());
+
+            int[] resArr = rotateArr(arr, k);
+            Console.WriteLine("\nThe rotated array looks like this: ");
+            foreach(int el in resArr)
+                Console.Write($"{el} ");
+        }
+
+        static int[] rotateArr(int[] arr, int k)
+        {
+            if (k > arr.Length || k < 0) return arr;
+
+            List<int> x = new List<int>();
+            x.AddRange(arr[k..]);
+            x.AddRange(arr.Take(k));
+
+            return x.ToArray();
+        }
+    }
+}
+
+/*
+ * This problem was asked by Amazon.
+Given an array and a number k that's smaller than the length of the array, rotate the array to the right k elements in-place.
+ */
