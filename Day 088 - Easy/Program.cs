061775d1f2ecf8d6705fd5530f153b393ceb3f42
dimitri-dev <maric.neven03@gmail.com>
2021-01-21 14:34:21 +0100

    Day 88 - solved
    Solved issue 88 - column int to string excel style

---
+++ Day 088 - Easy/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace _100daysCoding
+{
+    class Program_Day88
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input column number: ");
+            int col = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Integer value of column: {getCol(col)}");
+        }
+
+        const string alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        static string getCol(int c)
+        {
+            string col = "";
+            while (c > 0)
+            {
+                if (c % 26 == 0)
+                {
+                    col += "Z";
+                    c /= 26; c--;
+                }
+                else
+                {
+                    col += alpha[c % 26 - 1];
+                    c /= 26;
+                }
+            }
+
+            return new string(col.Reverse().ToArray());            
+        }
+    }
+}
