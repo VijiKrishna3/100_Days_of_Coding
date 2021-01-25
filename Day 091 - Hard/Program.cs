36a61d4c65d5e7bdce5d34983a06147d5746304a
dimitri-dev <maric.neven03@gmail.com>
2021-01-25 23:42:15 +0100

    Day 91 - solved
    Solved issue 91 - balance a paranthasis string with minimum number of insertions or deletions

---
+++ Day 091 - Hard/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace _100daysCoding
+{
+    class Program_Day91
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input a string: ");
+            string param = Console.ReadLine();
+
+            Console.WriteLine($"Balanced string would look like {balance(param)}.");
+        }
+
+        static string balance(string p)
+        {
+            int[] x = new int[] { p.Count(x => x == '('), p.Count(x => x == ')') };
+            int totalBracketsMissing = Math.Max(x[0], x[1]);
+
+            return string.Concat(Enumerable.Repeat("()", totalBracketsMissing));
+        }
+    }
+}
