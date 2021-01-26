af38e76eb8244684117352a7c00c67617d19d5af
dimitri-dev <maric.neven03@gmail.com>
2021-01-26 10:03:36 +0100

    Day 92 - solved
    Solved issue 92 - found the number of elements that are smaller than the current element

---
+++ Day 092 - Medium/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace _100daysCoding
+{
+    class Program_Day92
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter line of numbers: ");
+            string[] ins = Console.ReadLine().Split(' ');
+            int[] arr = (from entry in ins select Convert.ToInt32(entry)).ToArray();
+
+            int[] ret = findSmallerValues(arr);
+            Console.WriteLine("\nThe returned array is: ");
+            foreach (int elem in ret)
+                Console.Write($"{elem} ");
+        }
+
+        static int[] findSmallerValues(int[] arr)
+        {
+            if (arr.Length == 0) return new int[] { };
+
+            int[] counts = new int[arr.Length];
+
+            for (int j = arr.Length - 1; j >= 0; --j)
+                for (int i = j + 1; i < arr.Length; ++i)
+                    if (arr[i] < arr[j])
+                        ++counts[j];
+
+            return counts;
+        }
+    }
+}
