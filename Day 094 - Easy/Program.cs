43b7566324fd67ef31196ce904da84a25ae3efcd
dimitri-dev <maric.neven03@gmail.com>
2021-01-28 21:38:05 +0100

    Day 94 - solved
    Solved issue 94 - the most ones in a row in a binary representation of a number.

---
+++ Day 094 - Easy/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _100daysCoding
+{
+    class Program_Day94
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.Write("Enter a number: ");
+            int.TryParse(Console.ReadLine(), out n);
+
+            Console.WriteLine($"There are at most {nOnes(n)} 1s in a row .");
+        }
+
+        static int nOnes(int n)
+        {
+            string x = Convert.ToString(n, 2);
+            int maxCount = 0, currentCount = 0;
+            foreach (char q in x)
+                if (q == '1') ++currentCount;
+                else { maxCount = (maxCount > currentCount) ? maxCount : currentCount; currentCount = 0; }
+
+            return maxCount;
+        }
+    }
+}
